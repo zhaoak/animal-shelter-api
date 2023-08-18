95f5d0db9706a137df0407d99f37061135c05032
Allie Zhao <allie.k.zhao@gmail.com>
2023-08-18 16:02:18 -0700

    Add versioning library, configured in Program.cs

--- AnimalShelterAPI/Program.cs
+++ AnimalShelterAPI/Program.cs
@@ -14,6 +14,15 @@ builder.Services.AddDbContext<AnimalShelterAPIContext>(
                   )
                 );
 
+builder.Services.AddApiVersioning(opt =>
+                    {
+                        opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1,0);
+                        opt.AssumeDefaultVersionWhenUnspecified = true;
+                        opt.ReportApiVersions = true;
+                        opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                                        new HeaderApiVersionReader("x-api-version"),
+                                                                        new MediaTypeApiVersionReader("x-api-version"));
+                    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
